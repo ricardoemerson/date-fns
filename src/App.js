fa52bafed00371c1616583d20f3f70e4dc5582d5
Andréia Crystina <andreia.crystina@hotmail.com>
2020-01-15 00:39:21 -0300

    Adicionados sites para consultas.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,11 @@ function App() {
         onClick={ handleChangeDate }
       >Change Date for Initial Time</button>
 
+      <p><a href="https://blog.rocketseat.com.br/datas-e-horarios-javascript-date-fns-moment/">https://blog.rocketseat.com.br/datas-e-horarios-javascript-date-fns-moment/</a></p>
+      <p><a href="https://javascript-conference.com/blog/why-you-should-use-date-fns-for-manipulating-dates-with-javascript/">https://javascript-conference.com/blog/why-you-should-use-date-fns-for-manipulating-dates-with-javascript/</a></p>
+      <p><a href="https://dev.to/marinamosti/-formatting-and-modifying-javascript-dates-with-date-fns-3df2">https://dev.to/marinamosti/-formatting-and-modifying-javascript-dates-with-date-fns-3df2</a></p>
+      <p><a href="https://medium.com/@billys.moustakas/node-js-date-fns-good-date-handling-in-javascript-d78710c66fe0">https://medium.com/@billys.moustakas/node-js-date-fns-good-date-handling-in-javascript-d78710c66fe0</a></p>
+
     </div>
   );
 }
